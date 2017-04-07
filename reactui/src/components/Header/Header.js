db79df1039a32a4ab31291f5cd5d3fc4b9ebe4ed
Tong Zhao <zhaotong116@gmail.com>
2017-04-06 23:12:46 -0400

    React bootstrap header

--- reactui/src/components/Header/Header.js
+++ reactui/src/components/Header/Header.js
@@ -1,27 +1,38 @@
 import React from 'react'
 import { Link } from 'react-router'
-import './Header.css'
+import {Navbar, Nav, NavItem, MenuItem, NavDropdown} from 'react-bootstrap'
 
-const Header = ({ isAuthenticated, profile, error, onLoginClick, onLogoutClick }) =>
-  <div>
-    <h1>React Redux Auth0 Kit</h1>
-    <ul className="list-inline">
-      <li><Link to='/'>Home</Link></li>
-      <li><Link to='/about'>About</Link></li>
-    </ul>
-    { !isAuthenticated ? (
-      <button onClick={onLoginClick}>Login</button>
-    ) : (
-      <div>
-        <img src={profile.picture} height="40px" />
-        <span>Welcome, {profile.nickname}</span>
-        <button onClick={onLogoutClick}>Logout</button>
-      </div>
-    )}
-    { error &&
-      <p>{error}</p>
-    }
-  </div>
+const Header = ({ isAuthenticated, profile, onLoginClick, onLogoutClick }) => {
+  return (
+    <Navbar inverse>
+      <Navbar.Header>
+        <Navbar.Brand>
+          <a href="#">TicketDisputer</a>
+        </Navbar.Brand>
+        <Navbar.Toggle />
+      </Navbar.Header>
+      <Navbar.Collapse>
+        <Nav>
+          <NavItem eventKey={1} href="#"><Link to='/'>Home</Link></NavItem>
+          <NavItem eventKey={2} href="#"><Link to='/about'>About</Link></NavItem>
+
+        </Nav>
+        <Nav pullRight>
+          {!isAuthenticated ?
+            (<NavItem eventKey={1} onClick={onLoginClick}>Login</NavItem>) :
+            (
+              <NavDropdown eventKey={3} title={<img src={profile.picture} height="28px"/>} id="basic-nav-dropdown">
+                <MenuItem eventKey={3.1}>My cases</MenuItem>
+                <MenuItem eventKey={3.2}>New Case</MenuItem>
+                <MenuItem eventKey={3.3}>Profile</MenuItem>
+                <MenuItem divider/>
+                <MenuItem eventKey={3.4} onClick={onLogoutClick}>Logout</MenuItem>
+              </NavDropdown>)
+          }
+        </Nav>
+      </Navbar.Collapse>
+    </Navbar>)
+}
 
 Header.propTypes = {
   isAuthenticated: React.PropTypes.bool.isRequired,
